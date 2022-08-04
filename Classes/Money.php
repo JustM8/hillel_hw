6b9fdd7eb9b251eb38e8d553d8a79921f14cc189
unknown <gav.sqrt@gmail.com>
2022-08-04 22:31:24 +0300

    hw[09] Money classes fix git request

--- Classes/Money.php
+++ Classes/Money.php
@@ -2,7 +2,56 @@
 
 namespace Classes;
 
+use InvalidArgumentException ;
+
 class Money
 {
+    private int|float $amount;
+    protected Currency $currency;
 
+    public function __construct(int|float $amount, Currency $currency)
+    {
+        $this->setAmount($amount);
+        $this->setCurrency($currency);
+    }
+
+    public function setAmount(int|float  $amount):void
+    {
+        $this->amount = $amount;
+    }
+    public function setCurrency(Currency $currency):void
+    {
+        $this->currency = $currency;
+    }
+
+    public function getAmount(): float|int
+    {
+        return $this->amount;
+    }
+
+    public function getCurrency():object
+    {
+        return $this->currency;
+    }
+
+    public function equals(object $a, object $b):string
+    {
+        $result['amount'] = "Amount {$b->amount} is greater";
+        if($a->amount>$b->amount){
+            $result['amount'] = "Amount {$a->amount} is greater than or equal to Amount {$b->amount}";
+        }
+        $result['currency'] = 'Currencies are different';
+        if($a->currency == $b->currency){
+            $result['currency'] = 'The currencies are identical';
+        }
+        return "Comparison result: {$result['amount']}, {$result['currency']}";
+    }
+
+    public function add(object $a, object $b):float
+    {
+        if($a->currency != $b->currency){
+             throw new InvalidArgumentException ('Currencies are different');
+        }
+        return $a->amount+$b->amount;
+    }
 }
--- index.php
+++ index.php
@@ -1,1 +1,28 @@
 <?php
+
+require_once 'Classes/Currency.php';
+require_once 'Classes/Money.php';
+
+use Classes\Currency;
+use Classes\Money;
+
+
+try {
+    $cur = new Currency('EUR');
+    $money = new Money(100.0,new Currency('EUR'));
+    $money2 = new Money(50.1,new Currency('EUR'));
+
+    echo $cur->getIsCode();
+    echo '<br>';
+    echo '<hr>';
+    var_dump($cur->equals('EUR','EUR'));
+    echo '<br>';
+    echo '<hr>';
+    print_r($money->equals(new Money(100.0,new Currency('EUR')),new Money(50.1,new Currency('EUR'))));
+    echo '<br>';
+    echo '<hr>';
+    print_r($money->add(new Money(100.0,new Currency('EUR')),new Money(50.1,new Currency('EUR'))));
+
+}catch (InvalidArgumentException  $e){
+    echo "Error message: {$e->getMessage()}";
+}
