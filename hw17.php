daa459f0cb2c43dd61348f1e684b90de2e6aaca4
unknown <gav.sqrt@gmail.com>
2022-08-27 12:18:10 +0300

    hw[17] Interface segregation

---
+++ hw17.php
@@ -0,0 +1,30 @@
+<?php
+
+interface Eat
+{
+    public function eat();
+}
+interface Fly
+{
+    public function fly();
+}
+
+class Swallow implements Eat, Fly
+{
+    public function eat()
+    {
+
+    }
+    public function fly()
+    {
+
+    }
+}
+
+class Ostrich implements Eat
+{
+    public function eat()
+    {
+
+    }
+}
